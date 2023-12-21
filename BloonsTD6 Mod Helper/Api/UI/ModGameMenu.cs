4ac6754f4f621553517b17d63c64b376eb2b1649
awumii <pawel.js@protonmail.com>
2023-12-21 20:29:17 +0100

    Try linux events fix guess

--- BloonsTD6 Mod Helper/Api/UI/ModGameMenu.cs
+++ BloonsTD6 Mod Helper/Api/UI/ModGameMenu.cs
@@ -156,6 +156,7 @@ public abstract class ModGameMenu : ModContent
         foreach (var type in Types.Keys)
         {
             if (MelonUtils.IsUnderWineOrSteamProton() && type == typeof(AchievementsScreen)) continue;
+            if (MelonUtils.IsUnderWineOrSteamProton() && type == typeof(GameEventsScreen)) continue;
 
             var data = DataNames.GetValueOrDefault(type, "data");
             try
